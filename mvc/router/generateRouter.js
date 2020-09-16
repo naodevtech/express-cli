634ed172f7cf5b0a9feb7daedcc6eb26758446c8
naimtahouri <naimtahouri@gmail.com>
2020-09-17 01:09:43 +0200

    updating pattern path

--- mvc/router/generateRouter.js
+++ mvc/router/generateRouter.js
@@ -6,7 +6,7 @@ function CreateRouterService(routerMethod) {
   if (process.argv[2] === undefined) {
     return console.log(colors.red('Sorry enter name of file'));
   }
-  if (fs.existsSync(`./routes/${process.argv[2]}.js`)) {
+  if (fs.existsSync(`../routes/${process.argv[2]}.js`)) {
     return console.error(
       colors.red(
         `Sorry the route and the controller ${process.argv[2]} already exist!`
